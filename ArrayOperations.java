65b1b1a210683620f03549237fa92a723fad78ba
Edgar Ochoa <eom3108@gmail.com>
2020-06-27 20:32:10 -0500

    starting project

---
+++ ArrayOperations.java
@@ -0,0 +1,17 @@
+/**
+ * library for array operations
+ * @author edgar ochoa 
+ * @version 1.0.0
+ */
+
+public class ArrayOperations{
+
+    //Print Array 
+    public static void printArray(int array[], int size){
+        for(int i=0; i < size; i++){
+            System.out.println(s);
+        }
+    }
+}
+
+
